18db6e02b8bcdca17083043239920b85a18a6b4c
alan-damian <111703724+alan-damian@users.noreply.github.com>
2023-02-08 12:18:14 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # CODE:40
-#import interfaz
+import interfaz
 
 import csv
 import random
@@ -7,20 +7,27 @@ import interfaz
 
 def leer_palabra_secreta(palabras):
     with open(palabras, 'r') as csvfile:
-        lista_palabras = list(csv.reader(csvfile)) 
-        palabra_aleatoria = random.choice(lista_palabras)
-        return palabra_aleatoria
+        lista_palabras = list(csv.DictReader(csvfile)) 
+
+    for palabra in lista_palabras:
+        lista_palabras_oculta = palabra['palabras']
+        print(lista_palabras_oculta)
+    
+    palabra_aleatoria = random.choice(lista_palabras)
+    palabra_secreta = str(palabra_aleatoria['palabras'])
+    return palabra_secreta
 
 def pedir_letra(letras_usadas):
     while True:
-        letra = input('ingrese una letra: ').lower()
+        letra = str(input('ingrese una letra: ')).lower()
         if len(letra) != 1:
             print('introduce una letra sola.')
         elif letra in letras_usadas:
-            print('ingrese otra letra.')
+            pass
         else:
             letras_usadas.append(letra)
-            return letra
+            break
+    return letra
         
                  
 
@@ -54,7 +61,7 @@ if __name__ == "__main__":
     # la cantidad de letras de la palabra a adivinar.
     interfaz.dibujar(palabra_secreta, letras_usadas, intentos)
     
-    while intentos < max_cantidad_intentos = 7 and not es_ganador:
+    while intentos < max_cantidad_intentos == 7 and not es_ganador:
         # Pedir una nueva letra
         letra = pedir_letra(letras_usadas)
 
